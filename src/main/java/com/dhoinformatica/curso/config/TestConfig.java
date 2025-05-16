75ce5f751123875b9ac95ef8f2d0d84c42ecff6a
DyhegoS <dyhegodasilva@gmail.com>
2025-05-15 21:42:23 -0300

    Associacao muitos para muitos e JoinTable

--- src/main/java/com/dhoinformatica/curso/config/TestConfig.java
+++ src/main/java/com/dhoinformatica/curso/config/TestConfig.java
@@ -2,7 +2,7 @@ package com.dhoinformatica.curso.config;
 
 import java.time.Instant;
 import java.util.Arrays;
-
+import com.dhoinformatica.curso.resources.ProductResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +21,8 @@ import com.dhoinformatica.curso.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
+
+    private ProductResource productResource;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -33,6 +35,10 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+
+    TestConfig(ProductResource productResource) {
+        this.productResource = productResource;
+    }
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -50,6 +56,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+				
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
